a1356ebacef929ad2e4dc406bbe80e4bdd90644e
ivant8k <13523129@std.stei.itb.ac.id>
2026-05-09 02:58:30 +0700

    feat: implemented conv2d (non-shared)

--- src/cnn/layers/conv2d.py
+++ src/cnn/layers/conv2d.py
@@ -17,7 +17,6 @@ class Conv2DLayer:
         self.kH, self.kW, self.C_in, self.C_out = self.kernel.shape
         
     # Padding helper (valid vs same)
-
     def _get_pad(self, H, W):
         if self.padding == 'valid':
             return 0,0
@@ -71,11 +70,31 @@ class Conv2DLayer:
 class LocallyConnected2DLayer:
     def __init__(self, keras_layer):
         # TODO: load bobot (harusnya masih sama kayak conv2d)
-        pass
+        weights = keras_layer.get_weights()
+        self.kernel = weights[0]
+        self.bias = weights[1]
 
-    def forward(self):
-        # TODO: Implement forward pass
-        pass
+        cfg = keras_layer.get_config()
+        self.kH, self.kW = cfg["kernel_size"]
+        self.stride = cfg['strides'][0]
+        self.activation = cfg['activation']
+
+    def forward(self, x: np.ndarray) -> np.ndarray:
+        H, W, C_in = x.shape
+        out_H = (H - self.kH) // self.stride + 1
+        out_W = (W - self.kW) // self.stride + 1
+        out = np.zeros((out_H, out_W, self.kernel.shape[-1]))
+
+        for i in range(out_H):
+            for j in range(out_W):
+                pos_idx = i * out_W + j
+                patch = x[i*self.stride : i * self.stride+self.kH,
+                          j*self.stride : j * self.stride+self.kW, :]
+                patch_flat = patch.ravel()
+
+                out[i, j, :] = np.dot(patch_flat, self.kernel[:, pos_idx]) + self.bias
+
+        return self._apply_activation(out)
 
     # TODO: Implement backward pass (BONUS)
     def backward(self):
