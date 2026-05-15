05e43520c48f80f31a460f29ad245f0fded5ba19
ivant8k <13523129@std.stei.itb.ac.id>
2026-05-15 13:54:16 +0700

    feat: final notebook locally connected

--- src/cnn/layers/locally_connected.py
+++ src/cnn/layers/locally_connected.py
@@ -14,25 +14,49 @@ class LocallyConnected2DLayer:
 
         self.kH, self.kW = cfg["kernel_size"]
         self.stride = cfg['strides'][0]
+        self.padding = cfg.get('padding', 'valid')
         self.activation = cfg['activation']
+
+    def _get_pad(self, H, W):
+        if self.padding == 'valid':
+            return 0, 0
+        pad_h = max((H - 1) * self.stride + self.kH - H, 0)
+        pad_w = max((W - 1) * self.stride + self.kW - W, 0)
+
+        return pad_h // 2, pad_w // 2
 
     def forward(self, x: np.ndarray) -> np.ndarray:
         H, W, C_in = x.shape
+        
+        pad_h, pad_w = self._get_pad(H, W)
+        if pad_h > 0 or pad_w > 0:
+            x = np.pad(x, ((pad_h, pad_h), (pad_w, pad_w), (0, 0)), mode="constant")
+            H, W = x.shape[:2]
+
         out_H = (H - self.kH) // self.stride + 1
         out_W = (W - self.kW) // self.stride + 1
+        
+        expected_output_pixels = out_H * out_W
+        assert self.kernel.shape[0] == expected_output_pixels, \
+            f"Mismatch: Kernel memiliki {self.kernel.shape[0]} posisi, tetapi output butuh {expected_output_pixels}"
+
         out = np.zeros((out_H, out_W, self.kernel.shape[-1]))
 
         for i in range(out_H):
             for j in range(out_W):
                 pos_idx = i * out_W + j
-                patch = x[i*self.stride : i * self.stride+self.kH,
-                          j*self.stride : j * self.stride+self.kW, :]
+                
+                patch = x[i*self.stride : i*self.stride + self.kH,
+                          j*self.stride : j*self.stride + self.kW, :]
+                
                 patch_flat = patch.ravel()
+                
                 out[i, j, :] = np.dot(patch_flat, self.kernel[pos_idx]) + self.bias[pos_idx]
 
         return self._apply_activation(out)
 
     def _apply_activation(self, x):
+        """Abstraksi fungsi aktivasi."""
         if self.activation == 'relu':
             return Activation().relu(x)
         elif self.activation == 'sigmoid':
@@ -44,6 +68,6 @@ class LocallyConnected2DLayer:
         else:
             return x
 
-    # TODO: Implement backward pass (BONUS)
     def backward(self):
+        # BONUS: Belum diimplementasikan
         pass
